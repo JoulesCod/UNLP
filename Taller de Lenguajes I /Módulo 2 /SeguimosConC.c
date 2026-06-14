29d191766384aada3ec5d604b5d8d3291d37149f
Ju <julianamoroni.m@gmail.com>
2026-06-13 23:37:32 -0300

    Delete Taller de Lenguajes I /Módulo 2  directory

--- Taller de Lenguajes I /Módulo 2 /SeguimosConC.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("No Prestar Atenciónnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn\n");
-    return 0;
-}
