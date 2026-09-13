3350f9c70a223289024d9c7e27ffdd0161f646c6
Joules (Ju) <julianamoroni.m@gmail.com>
2026-09-13 01:28:40 -0300

    Update VemosIntroducciónJava.java

--- Progra II /VemosIntroducciónJava.java
+++ Progra II /VemosIntroducciónJava.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("no le den tención a esto");
+        System.out.println("no le den atención a esto");
     }
 }
