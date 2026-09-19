72b65e42f285f2c46b82ca0fe56a4e72759ad9b6
Joules (Ju) <julianamoroni.m@gmail.com>
2026-09-19 14:25:45 -0300

    Update HacemosProyectosEnJava.java

--- Taller de Lenguajes II/HacemosProyectosEnJava.java
+++ Taller de Lenguajes II/HacemosProyectosEnJava.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("... what?");
+        System.out.println("i'm");
+        System.out.println("doing this");
+        System.out.println("cause I've to study");
     }
 }
